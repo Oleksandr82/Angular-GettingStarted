8f46eb2291231bbe73fb61285a15c7e67eadbf46
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2021-10-31 19:13:18 +0100

    Added a servic to retrieve a list of (hardcoded) products

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -12,44 +13,26 @@ export class ProductListComponent implements OnInit {
     imageWidth = 50;
     imageMargin = 2;
     showImage = false
-
+    filteredProducts: IProduct[] = [];
     private _listFilter = '';
+
+    constructor(private productService: ProductService) {
+    }
+
+    products: IProduct[] = [];
+    
     get listFilter(): string {
         return this._listFilter;
     }
+    
     set listFilter(value: string) {
         this._listFilter = value;
         this.filteredProducts = this.filterProducts(value)
     }
 
-    filteredProducts: IProduct[] = [];
-
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2021",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2021",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        }
-    ];
-
     ngOnInit(): void {
-        this._listFilter = 'cart';
-        this.filteredProducts = this.filterProducts(this._listFilter);
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
     
     filterProducts(filterBy: string): IProduct[] {
