d570429219b6e4914907c4d836a1be92076e4b5e
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2021-11-01 20:48:39 +0100

    Request prodct data via HTTP (service + observable)

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -7,7 +8,7 @@ import { ProductService } from "./product.service";
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
 
     pageTitle = 'Product List';
     imageWidth = 50;
@@ -15,6 +16,8 @@ export class ProductListComponent implements OnInit {
     showImage = false
     filteredProducts: IProduct[] = [];
     private _listFilter = '';
+    errorMessage = '';
+    sub!: Subscription;
 
     constructor(private productService: ProductService) {
     }
@@ -31,8 +34,17 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
     
     filterProducts(filterBy: string): IProduct[] {
